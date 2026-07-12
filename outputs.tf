aad74f0108812a499f29ee5a0baa6f5840b6caec
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,22 @@
-output "application_insights_api_keys" {
-  description = "All application_insights_api_key resources"
-  value       = azurerm_application_insights_api_key.application_insights_api_keys
-  sensitive   = true
-}
 output "application_insights_api_keys_api_key" {
-  description = "List of api_key values across all application_insights_api_keys"
-  value       = [for k, v in azurerm_application_insights_api_key.application_insights_api_keys : v.api_key]
+  description = "Map of api_key values across all application_insights_api_keys, keyed the same as var.application_insights_api_keys"
+  value       = { for k, v in azurerm_application_insights_api_key.application_insights_api_keys : k => v.api_key }
   sensitive   = true
 }
 output "application_insights_api_keys_application_insights_id" {
-  description = "List of application_insights_id values across all application_insights_api_keys"
-  value       = [for k, v in azurerm_application_insights_api_key.application_insights_api_keys : v.application_insights_id]
+  description = "Map of application_insights_id values across all application_insights_api_keys, keyed the same as var.application_insights_api_keys"
+  value       = { for k, v in azurerm_application_insights_api_key.application_insights_api_keys : k => v.application_insights_id }
 }
 output "application_insights_api_keys_name" {
-  description = "List of name values across all application_insights_api_keys"
-  value       = [for k, v in azurerm_application_insights_api_key.application_insights_api_keys : v.name]
+  description = "Map of name values across all application_insights_api_keys, keyed the same as var.application_insights_api_keys"
+  value       = { for k, v in azurerm_application_insights_api_key.application_insights_api_keys : k => v.name }
 }
 output "application_insights_api_keys_read_permissions" {
-  description = "List of read_permissions values across all application_insights_api_keys"
-  value       = [for k, v in azurerm_application_insights_api_key.application_insights_api_keys : v.read_permissions]
+  description = "Map of read_permissions values across all application_insights_api_keys, keyed the same as var.application_insights_api_keys"
+  value       = { for k, v in azurerm_application_insights_api_key.application_insights_api_keys : k => v.read_permissions }
 }
 output "application_insights_api_keys_write_permissions" {
-  description = "List of write_permissions values across all application_insights_api_keys"
-  value       = [for k, v in azurerm_application_insights_api_key.application_insights_api_keys : v.write_permissions]
+  description = "Map of write_permissions values across all application_insights_api_keys, keyed the same as var.application_insights_api_keys"
+  value       = { for k, v in azurerm_application_insights_api_key.application_insights_api_keys : k => v.write_permissions }
 }
 
